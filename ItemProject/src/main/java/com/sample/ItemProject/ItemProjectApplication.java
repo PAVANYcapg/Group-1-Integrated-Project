0578c3c257b86a65026c015d958c1bbc0cfcc1be
Capgemini grp acc <101783613+PAVANYcapg@users.noreply.github.com>
2022-04-19 11:18:10 +0530

    Add files via upload
    ipl/pavanadminservices

---
+++ ItemProject/src/main/java/com/sample/ItemProject/ItemProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sample.ItemProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemProjectApplication.class, args);
+	}
+
+}
